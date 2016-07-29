d55b9c486be4eaa565013acf63f174652aae0875
mr_liu_easy_ios刘勋鹏 <137973550@qq.com>
2016-07-29 16:14:36 +0800

    Initial Commit

---
+++ 倒计时time/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  倒计时time
+//
+//  Created by chuangqu on 16/7/29.
+//  Copyright © 2016年 mr.liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 倒计时time/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  倒计时time
+//
+//  Created by chuangqu on 16/7/29.
+//  Copyright © 2016年 mr.liu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
